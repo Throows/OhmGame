c26e359e809ce7938b6fc476ddac9494a451e085
Throows <throowsdev@gmail.com>
2024-09-08 22:52:49 +0200

    Add log location

--- OhmEngine/include/OhmEngine/Core/Logger.hpp
+++ OhmEngine/include/OhmEngine/Core/Logger.hpp
@@ -20,16 +20,16 @@ namespace OHE
             static std::shared_ptr<spdlog::logger> s_GameLogger;
     };
 
-#define OHE_ENGINE_TRACE(...)    ::OHE::Logger::GetEngineLogger()->trace(__VA_ARGS__)
-#define OHE_ENGINE_INFO(...)     ::OHE::Logger::GetEngineLogger()->info(__VA_ARGS__)
-#define OHE_ENGINE_WARN(...)     ::OHE::Logger::GetEngineLogger()->warn(__VA_ARGS__)
-#define OHE_ENGINE_ERROR(...)    ::OHE::Logger::GetEngineLogger()->error(__VA_ARGS__)
-#define OHE_ENGINE_CRITICAL(...) ::OHE::Logger::GetEngineLogger()->critical(__VA_ARGS__)
+#define OHE_ENGINE_TRACE(...)    ::OHE::Logger::GetEngineLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::trace, __VA_ARGS__)
+#define OHE_ENGINE_INFO(...)     ::OHE::Logger::GetEngineLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::info, __VA_ARGS__)
+#define OHE_ENGINE_WARN(...)     ::OHE::Logger::GetEngineLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::warn, __VA_ARGS__)
+#define OHE_ENGINE_ERROR(...)    ::OHE::Logger::GetEngineLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::err, __VA_ARGS__)
+#define OHE_ENGINE_CRITICAL(...) ::OHE::Logger::GetEngineLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::critical, __VA_ARGS__)
 
-#define OHE_GAME_TRACE(...)    ::OHE::Logger::GetGameLogger()->trace(__VA_ARGS__)
-#define OHE_GAME_INFO(...)     ::OHE::Logger::GetGameLogger()->info(__VA_ARGS__)
-#define OHE_GAME_WARN(...)     ::OHE::Logger::GetGameLogger()->warn(__VA_ARGS__)
-#define OHE_GAME_ERROR(...)    ::OHE::Logger::GetGameLogger()->error(__VA_ARGS__)
-#define OHE_GAME_CRITICAL(...) ::OHE::Logger::GetGameLogger()->critical(__VA_ARGS__)
+#define OHE_GAME_TRACE(...)    ::OHE::Logger::GetGameLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::trace, __VA_ARGS__)
+#define OHE_GAME_INFO(...)     ::OHE::Logger::GetGameLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::info, __VA_ARGS__)
+#define OHE_GAME_WARN(...)     ::OHE::Logger::GetGameLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::warn, __VA_ARGS__)
+#define OHE_GAME_ERROR(...)    ::OHE::Logger::GetGameLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::err, __VA_ARGS__)
+#define OHE_GAME_CRITICAL(...) ::OHE::Logger::GetGameLogger()->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::critical, __VA_ARGS__)
 
 } // namespace OHE
